8d77c8c869b7cd54cf691c352de791c888dbadea
Greg Heartsfield <scsibug@imap.cc>
2020-10-31 15:02:50 -0500

    Use bloom filter to prevent downloading same image twice

--- get_flickr_photoset.py
+++ get_flickr_photoset.py
@@ -1,4 +1,4 @@
-# Synchronize a Flickr Album locally, and convert for e-ink display.
+# Download and process files from flickr group
 import flickrapi
 import requests
 import hashlib
@@ -7,6 +7,7 @@ import configparser
 from timeit import default_timer as timer
 from pprint import pprint
 import tempfile
+from bloom_filter import BloomFilter
 
 config = configparser.ConfigParser()
 config.read('pictureframe.config')
@@ -20,6 +21,7 @@ group_id = flickr_config['Group']
 file_config = config['FILES']
 display_dir = file_config['Active']
 max_queue_size = file_config.getint('MaxQueue', fallback=100)
+bloom_filter_file = file_config.get('BloomFilterFile', fallback='dl.cache')
 
 screen_config = config['SCREEN']
 colors = screen_config["Colors"]
@@ -33,6 +35,8 @@ if flipflop:
 
 flickr = flickrapi.FlickrAPI(api_key, api_secret)
 
+seen_filter = BloomFilter(max_elements=10000, error_rate=0.01, filename=bloom_filter_file)
+
 def process_photo(url, destination):
     print("Processing {}".format(url))
     # Determine if file exists in destination
@@ -41,7 +45,6 @@ def process_photo(url, destination):
     if (not os.path.exists(dest_file_path)):
         # Download file to temporary location
         with tempfile.NamedTemporaryFile(mode='w+b', prefix="flickr-group-", delete=True) as dl_file:
-            print("Saving {} ...".format(url))
             start = timer()
             r = requests.get(url)
             end = timer()
@@ -51,9 +54,10 @@ def process_photo(url, destination):
             start = timer()
             # Removed -colorspace Gray
             os.system("convert {} -colorspace gray -ordered-dither o8x8 -colors {} -contrast -contrast -gamma {} -resize {}x{} -gravity center -extent {}x{} {} {}".format(dl_file.name, colors, gamma, width, height, width, height, flipflop_cmd, dest_file_path))
-            print("Destination is {}".format(dest_file_path))
             end = timer()
             print("Converted in {} seconds".format(round(end-start,1)))
+            # Store this file in local database
+            
 
 # If the group queue directory is full, exit.
 display_dir_size = len([name for name in os.listdir(display_dir) if os.path.isfile(name)])
@@ -67,12 +71,16 @@ print(group_id)
 group_photo_page = flickr.groups.pools.getPhotos(group_id=group_id, per_page=100, extras='url_o, url_k')
 for results in group_photo_page:
     for photo in results:
-        print("Downloading photo '{}'".format(photo.get('title')))
-        print("Photo url is {}".format(photo.get('urls')))
+        print("Checking photo '{}'".format(photo.get('title')))
         urls = [photo.get('url_o'), photo.get('url_k')]
         url = next((item for item in urls if item is not None), None)
         if url:
-            process_photo(url, display_dir)
+            # Check if we have downloaded this before
+            if url in seen_filter:
+                print("Skipping - downloaded this previously")
+            else:
+                seen_filter.add(url)
+                process_photo(url, display_dir)
         else:
             print("Skipping - large image size not available")
 exit()
