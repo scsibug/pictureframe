614be8fd807404c05f9dbeda142be660a4f664ac
Greg Heartsfield <scsibug@imap.cc>
2020-10-31 14:47:46 -0500

    display_once script now works

--- display_once.sh
+++ display_once.sh
@@ -1,6 +1,8 @@
 # Display the oldest file, then delete it.
-album_path = /home/pi/album
+# epd_write should be the IT8951 binary
+album_path=/home/pi/album
 f=$(ls -tc $album_path | head -n 1)
-if [[ -f $f ]]; then
+if [[ -f $album_path/$f ]]; then
     epd_write 0 0 $album_path/$f
     rm $album_path/$f
+fi
